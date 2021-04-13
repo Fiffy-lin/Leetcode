e9a2f6c7129721c7b33a6a7655fad41177b5bc15
Fifflaren <hallom4@126.com>
2021-04-13 11:03:53 +0800

    0413

---
+++ 109. 有序链表转换二叉搜索树.cpp
@@ -0,0 +1,68 @@
+// https://leetcode-cn.com/problems/convert-sorted-list-to-binary-search-tree/
+
+// 给定一个单链表，其中的元素按升序排序，将其转换为高度平衡的二叉搜索树。
+
+// 本题中，一个高度平衡二叉树是指一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1。
+
+// 示例:
+
+// 给定的有序链表： [-10, -3, 0, 5, 9],
+
+// 一个可能的答案是：[0, -3, 9, -10, null, 5], 它可以表示下面这个高度平衡二叉搜索树：
+
+//       0
+//      / \
+//    -3   9
+//    /   /
+//  -10  5
+// 通过次数77,204提交次数101,248
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* getMedian(ListNode* left, ListNode* right) {
+        ListNode* fast = left;
+        ListNode* slow = left;
+        while (fast != right && fast->next != right) {
+            fast = fast->next;
+            fast = fast->next;
+            slow = slow->next;
+        }
+        return slow;
+    }
+
+    TreeNode* buildTree(ListNode* left, ListNode* right) {
+        if (left == right) {
+            return nullptr;
+        }
+        ListNode* mid = getMedian(left, right);
+        TreeNode* root = new TreeNode(mid->val);
+        root->left = buildTree(left, mid);
+        root->right = buildTree(mid->next, right);
+        return root;
+    }
+
+    TreeNode* sortedListToBST(ListNode* head) {
+        return buildTree(head, nullptr);
+    }
+};
---
+++ 127. 单词接龙.cpp
@@ -0,0 +1,94 @@
+// https://leetcode-cn.com/problems/word-ladder
+
+// 字典 wordList 中从单词 beginWord 和 endWord 的 转换序列 是一个按下述规格形成的序列：
+
+// 序列中第一个单词是 beginWord 。
+// 序列中最后一个单词是 endWord 。
+// 每次转换只能改变一个字母。
+// 转换过程中的中间单词必须是字典 wordList 中的单词。
+// 给你两个单词 beginWord 和 endWord 和一个字典 wordList ，找到从 beginWord 到 endWord 的 最短转换序列 中的 单词数目 。如果不存在这样的转换序列，返回 0。
+
+//  
+// 示例 1：
+
+// 输入：beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log","cog"]
+// 输出：5
+// 解释：一个最短转换序列是 "hit" -> "hot" -> "dot" -> "dog" -> "cog", 返回它的长度 5。
+// 示例 2：
+
+// 输入：beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log"]
+// 输出：0
+// 解释：endWord "cog" 不在字典中，所以无法进行转换。
+//  
+
+// 提示：
+
+// 1 <= beginWord.length <= 10
+// endWord.length == beginWord.length
+// 1 <= wordList.length <= 5000
+// wordList[i].length == beginWord.length
+// beginWord、endWord 和 wordList[i] 由小写英文字母组成
+// beginWord != endWord
+// wordList 中的所有字符串 互不相同
+
+// 来源：力扣（LeetCode）
+// 链接：https://leetcode-cn.com/problems/word-ladder
+// 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+class Solution {
+public:
+    unordered_map<string, int> wordId;
+    vector<vector<int>> edge;
+    int nodeNum = 0;
+
+    void addWord(string& word) {
+        if (!wordId.count(word)) {
+            wordId[word] = nodeNum++;
+            edge.emplace_back();
+        }
+    }
+
+    void addEdge(string& word) {
+        addWord(word);
+        int id1 = wordId[word];
+        for (char& it : word) {
+            char tmp = it;
+            it = '*';
+            addWord(word);
+            int id2 = wordId[word];
+            edge[id1].push_back(id2);
+            edge[id2].push_back(id1);
+            it = tmp;
+        }
+    }
+
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        for (string& word : wordList) {
+            addEdge(word);
+        }
+        addEdge(beginWord);
+        if (!wordId.count(endWord)) {
+            return 0;
+        }
+        vector<int> dis(nodeNum, INT_MAX);
+        int beginId = wordId[beginWord], endId = wordId[endWord];
+        dis[beginId] = 0;
+
+        queue<int> que;
+        que.push(beginId);
+        while (!que.empty()) {
+            int x = que.front();
+            que.pop();
+            if (x == endId) {
+                return dis[endId] / 2 + 1;
+            }
+            for (int& it : edge[x]) {
+                if (dis[it] == INT_MAX) {
+                    dis[it] = dis[x] + 1;
+                    que.push(it);
+                }
+            }
+        }
+        return 0;
+    }
+};
